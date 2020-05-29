27e85c6043038aedd1da5468b1854165230b1878
amelie saccomandi <amelie.saccomandi@viacesi.fr>
2020-05-29 11:44:41 +0200

    interface login

--- StudyCESI.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ StudyCESI.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -45,7 +45,7 @@ namespace StudyCESI.Web.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Se souvenir de moi?")]
             public bool RememberMe { get; set; }
         }
 
