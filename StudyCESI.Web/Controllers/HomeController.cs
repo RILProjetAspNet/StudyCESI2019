d3ef4f373cf48fc215c3bdfbbc9caa05d6ba69c1
amelie saccomandi <amelie.saccomandi@viacesi.fr>
2020-05-29 17:56:46 +0200

    début dashboard etudiant

--- StudyCESI.Web/Controllers/HomeController.cs
+++ StudyCESI.Web/Controllers/HomeController.cs
@@ -6,13 +6,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StudyCESI.Web.Models;
 
+using Microsoft.EntityFrameworkCore;
+using StudyCESI.Model.Data;
+using StudyCESI.Model.Entities;
+using Microsoft.AspNetCore.Identity;
+
+
 namespace StudyCESI.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly StudyCesiContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(StudyCesiContext context, UserManager<User> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated && User.FindFirst("Role").Value == "Enseignant")
+            {
+                var exams = await _context.Exams.ToListAsync();
+                return View(exams);
+
+            }
+            else if (User.Identity.IsAuthenticated && User.FindFirst("Role").Value == "Etudiant")
+            {
+                // Get exams who User can pass
+                var exams = from exam in _context.Exams
+                            join userExam in _context.UserExams
+                                on exam.ExamId equals userExam.ExamId
+                            where userExam.NumberTries > 0
+                                && userExam.UserId == _userManager.GetUserId(User)
+                            select exam;
+
+                return View(exams);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public IActionResult About()
